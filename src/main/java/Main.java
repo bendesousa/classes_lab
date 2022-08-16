a0dc4757db4fa7e83ba38bce9a0761bb6382be58
bendesousa <bdsousa069@gmail.com>
2022-08-16 15:07:45 +0100

    finished rectangle class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
+//        Account Class test
         Account myAccount = new Account("1", "Johnson", 300);
         int myBalance = myAccount.getBalance();
 
         System.out.println(myBalance);
 
+//        Circle Class test
         Circle myCircle = new Circle(13);
         double radiusCircle = myCircle.getRadius();
         double circumferenceCircle = myCircle.getCircumference();
 
         System.out.println(radiusCircle);
         System.out.println(circumferenceCircle);
+
+//        Rectangle Class test
+        
     }
 
 
--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,2 +1,65 @@
 public class Rectangle {
+
+//    Properties
+    double length;
+    double width;
+    double perimeter;
+    double area;
+    double diagonal;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+        this.perimeter = (2 * length) + (2 * width);
+        this.area = length * width;
+        this.diagonal = Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2));
+    }
+
+//    Methods
+
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+
+    public void setPerimeter(double perimeter) {
+        this.perimeter = perimeter;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public double getDiagonal() {
+        return diagonal;
+    }
+
+    public void setDiagonal(double diagonal) {
+        this.diagonal = diagonal;
+    }
+
+    @Override
+    public String toString(){
+        return "Rectangle{" + "length='" + length + ", width=" + width + '}';
+    }
 }
