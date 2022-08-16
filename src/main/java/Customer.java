cb9390f0ebae8be029e0f6c1d28287beeb108490
bendesousa <bdsousa069@gmail.com>
2022-08-16 15:35:30 +0100

    add labelling to main class

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -8,7 +8,9 @@ public class Customer {
     String fullName;
 
     public Customer(int id, String firstName, String lastName, double heldMoney){
-        this.id = id;
+        int min = 1;
+        int max = 1000;
+        this.id = (int)Math.floor(Math.random() * (max - min + 1) + min);
         this.firstName = firstName;
         this.lastName = lastName;
         this.heldMoney = heldMoney;
@@ -16,6 +18,12 @@ public class Customer {
     }
 
 //    Methods
-    
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,15 @@ public class Main {
         Account myAccount = new Account("1", "Johnson", 300);
         int myBalance = myAccount.getBalance();
 
-        System.out.println(myBalance);
+        System.out.println("Account Balance: " + myBalance);
 
 //        Circle Class test
         Circle myCircle = new Circle(13);
         double radiusCircle = myCircle.getRadius();
         double circumferenceCircle = myCircle.getCircumference();
 
-        System.out.println(radiusCircle);
-        System.out.println(circumferenceCircle);
+        System.out.println("Radius of the Circle: " + radiusCircle);
+        System.out.println("Circumference of the Circle:" + circumferenceCircle);
 
 //        Rectangle Class test
         Rectangle myRectangle = new Rectangle(12, 6);
@@ -23,11 +23,11 @@ public class Main {
         double areaRectangle = myRectangle.getArea();
         double diagonalRectangle = myRectangle.getDiagonal();
 
-        System.out.println(lengthRectangle);
-        System.out.println(widthRectangle);
-        System.out.println(perimeterRectangle);
-        System.out.println(areaRectangle);
-        System.out.println(diagonalRectangle);
+        System.out.println("Length of the Rectangle: " + lengthRectangle);
+        System.out.println("Width of the Rectangle: " + widthRectangle);
+        System.out.println("Perimeter of the Rectangle: " + perimeterRectangle);
+        System.out.println("Area of the Rectangle: " + areaRectangle);
+        System.out.println("Diagonal of the Rectangle: " + diagonalRectangle);
     }
 
 
